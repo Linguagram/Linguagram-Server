81a1a26ead3544970f833b4c82ee331ff13370bf
frs18840@gmail.com <frs18840@gmail.com>
2023-01-02 07:26:02 +0700

    fix indentation

--- controllers/index.js
+++ controllers/index.js
@@ -28,64 +28,64 @@ class Controller {
     try {
       // Avatar ID belum ada
       const {
-	username,
-	email,
-	password,
-	country,
-	phoneNumber,
-	// Dipisah atau jadi satu array?
-	nativeLanguages = [],
-	interestLanguages = [],
+        username,
+        email,
+        password,
+        country,
+        phoneNumber,
+        // Dipisah atau jadi satu array?
+        nativeLanguages = [],
+        interestLanguages = [],
       } = req.body
 
       let createdUser;
 
       await sequelize.transaction(async (t) => {
-	// begin transaction
-	createdUser = await User.create({
-	  username,
-	  email,
-	  password,
-	  country,
-	  phoneNumber
-	}, {
-	    transaction: t
-	});
+        // begin transaction
+        createdUser = await User.create({
+          username,
+          email,
+          password,
+          country,
+          phoneNumber
+        }, {
+            transaction: t
+          });
 
-	// create user languages ===
-	const createUserLanguages = [
-	  ...nativeLanguages.map(lang => ({
-	    type: "native",
-	    UserId: createdUser.id,
-	    LanguageId: lang,
-	    })
-	  ),
-	  ...interestLanguages.map(lang => ({
-	    type: "interest",
-	    UserId: createdUser.id,
-	    LanguageId: lang,
-	    })
-	  ),
-	  // filter duplicates
-	  ].reduce((prev, val) =>
-	    prev.some(pval =>
-	      pval.LanguageId === val.LanguageId
-	      && pval.type === val.type)
-	    ? prev
-	    : prev.concat([val]),
-	    []);
+        // create user languages ===
+        const createUserLanguages = [
+          ...nativeLanguages.map(lang => ({
+            type: "native",
+            UserId: createdUser.id,
+            LanguageId: lang,
+          })
+          ),
+          ...interestLanguages.map(lang => ({
+            type: "interest",
+            UserId: createdUser.id,
+            LanguageId: lang,
+          })
+          ),
+          // filter duplicates
+        ].reduce((prev, val) =>
+          prev.some(pval =>
+            pval.LanguageId === val.LanguageId
+              && pval.type === val.type)
+            ? prev
+            : prev.concat([val]),
+          []);
 
-	const newUserLanguages = await UserLanguage.bulkCreate(createUserLanguages, {
-	  transaction: t,
-	});
+        const newUserLanguages = await UserLanguage.bulkCreate(createUserLanguages, {
+          transaction: t,
+        });
       });
 
       const opts = userFetchAttributes(Media);
       opts.include.push(
-	{
-	  model: UserLanguage,
-	  include: [Language],
-	}
+        {
+          model: UserLanguage,
+          include: [Language],
+        }
       );
 
       const newUser = await User.findByPk(createdUser.id, opts);
@@ -97,14 +97,14 @@ class Controller {
       sendMail(newUser.email, newUser.username, link)
 
       const payload = {
-	id: newUser.id,
+        id: newUser.id,
       };
 
       const access_token = signToken(payload)
 
       res.status(201).json({
-	access_token,
-	user: newUser,
+        access_token,
+        user: newUser,
       });
     } catch (err) {
       next(err)
@@ -116,39 +116,39 @@ class Controller {
       const {email, password} = req.body
 
       if(!email) throw {
-	status: 400,
-	message: 'Email is required',
+        status: 400,
+        message: 'Email is required',
       };
 
       if(!password) throw {
-	status: 400,
-	message: 'Password is required',
+        status: 400,
+        message: 'Password is required',
       };
 
       const loggedInUser = await User.findOne({where: {email}})
       if(!loggedInUser) throw {
-	status: 401,
-	message: 'Invalid email/password',
+        status: 401,
+        message: 'Invalid email/password',
       };
 
       const isValidPassword = verifyHash(password, loggedInUser.password)
       if(!isValidPassword) throw {
-	status: 401,
-	message: 'Invalid email/password',
+        status: 401,
+        message: 'Invalid email/password',
       };
 
       if(!loggedInUser.verified) {
-	const verificationId = signToken(loggedInUser.id)
-	const link = `http://localhost:3000/users/verify?verification=${verificationId}`
-	sendMail(loggedInUser.email, loggedInUser.username, link)
-	throw {
-	  status: 401,
-	  message: 'Email address has not been verified!',
-	};
+        const verificationId = signToken(loggedInUser.id)
+        const link = `http://localhost:3000/users/verify?verification=${verificationId}`
+        sendMail(loggedInUser.email, loggedInUser.username, link)
+        throw {
+          status: 401,
+          message: 'Email address has not been verified!',
+        };
       }
 
       const payload = {
-	id: loggedInUser.id
+        id: loggedInUser.id
       }
 
       const access_token = signToken(payload)
@@ -167,19 +167,19 @@ class Controller {
 
       const theSearchedUser = await User.findByPk(id)
       if(!theSearchedUser) throw {
-	status: 401,
-	message: 'Invalid Link',
+        status: 401,
+        message: 'Invalid Link',
       };
 
       if(theSearchedUser.verified) throw {
-	status: 400,
-	message: 'Your email address has been verified',
+        status: 400,
+        message: 'Your email address has been verified',
       };
 
       await User.update({ verified: true }, {
-	where: {
-	  id: theSearchedUser.id
-	}
+        where: {
+          id: theSearchedUser.id
+        }
       });
 
       res.status(200).json({message: `${theSearchedUser.email} has been verified`})
@@ -188,8 +188,8 @@ class Controller {
       next(err)
     }
   }
-
-
 }
 
 module.exports = Controller
+
+// vim: sw=2 ts=8 et
